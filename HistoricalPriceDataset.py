687a7f9f56fbc9662be922787a905c1b4725760d
skiddles <marks.technical@outlook.com>
2018-07-11 17:48:38 -0500

    Interim

--- HistoricalPriceDataset.py
+++ HistoricalPriceDataset.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 import os
 
+
 class HistPriceDataset(Dataset):
     """Historical Stock Price Dataset"""
     def __init__(self, csvfile, rootdir=None, log=False):
@@ -21,16 +22,15 @@ class HistPriceDataset(Dataset):
         self.log_transform = log
         self.roll_forward()
         self.calculate_differences()
-
-
+        self.date_labels, self.prices, self.change = self.torch_it()
 
     def __len__(self):
         return len(self.price_history)
 
     def __getitem__(self, idx):
-        date_label = self.price_history.iloc[idx, 0]
-        price = self.price_history.iloc[idx, 1]
-        change = self.price_history.iloc[idx, 2]
+        date_label = self.date_labels.iloc[idx, 0]
+        price = self.prices.iloc[idx, 1]
+        change = self.change.iloc[idx, 2]
 
         if self.log_transform:
             if self.log_transform == 10:
@@ -46,7 +46,9 @@ class HistPriceDataset(Dataset):
                 price = np.log10(price)
                 change = np.log10(change)
 
-        sample = {'date_label': date_label, 'price': price, 'change': change}
+        price_tensor = price
+        change_tensor = change
+        sample = {'date_label': date_label, 'price': price_tensor, 'change': change_tensor}
         return sample
 
     def roll_forward(self):
@@ -58,5 +60,12 @@ class HistPriceDataset(Dataset):
     def calculate_differences(self):
         self.price_history['change'] = None
         for i in range(1, len(self.price_history)):
-            self.price_history.iloc[i, 2] = self.price_history.iloc[i, 1] - self.price_history.iloc[i-1, 1]
+            self.price_history.iloc[i, 2] = np.float64((self.price_history.iloc[i-1, 1] - self.price_history.iloc[i, 1]) / self.price_history.iloc[i-1, 1])
         print('Computed periodic price change!')
+
+    def torch_it(self):
+        datelabels = self.price_history.iloc[:, 0]
+        prices = torch.from_numpy(np.array(self.price_history.iloc[:, 1]))
+        print(self.price_history['change'])
+        change = torch.from_numpy(np.array(self.price_history.iloc[:, 2]))
+        return datelabels, prices, change
--- SPY_LSTM.py
+++ SPY_LSTM.py
@@ -1,6 +1,6 @@
-import os
 from torch.utils.data import DataLoader
 import HistoricalPriceDataset as PriceData
+
 
 debug_level = 3
 pricedata = PriceData.HistPriceDataset(csvfile='./data/SP500.csv')
@@ -15,3 +15,7 @@ dataloader = DataLoader(pricedata,
                         batch_size=1,
                         shuffle=False,
                         num_workers=4)
+
+
+for i, x in enumerate(dataloader):
+    print(x)
