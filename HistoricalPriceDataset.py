f90d773cbc47692bfec0921be2b317fc2c58c801
skiddles <marks.technical@outlook.com>
2018-07-11 13:45:54 -0500

    Modified Dataset module:     1. Rolled forward prices when price is $0     2. Computed periodic difference

--- HistoricalPriceDataset.py
+++ HistoricalPriceDataset.py
@@ -19,24 +19,44 @@ class HistPriceDataset(Dataset):
         self.price_history = pd.read_csv(self.qual_filename)
         self.root_dir = rootdir
         self.log_transform = log
+        self.roll_forward()
+        self.calculate_differences()
+
+
 
     def __len__(self):
         return len(self.price_history)
 
     def __getitem__(self, idx):
-        date = self.price_history.iloc[idx, 1].as_matrix()
-        price = self.price_history.iloc[idx, 2].as_matrix()
+        date_label = self.price_history.iloc[idx, 0]
+        price = self.price_history.iloc[idx, 1]
+        change = self.price_history.iloc[idx, 2]
 
         if self.log_transform:
             if self.log_transform == 10:
                 price = np.log10(price)
+                change = np.log10(change)
             elif self.log_transform == 2:
                 price = np.log2(price)
+                change = np.log2(change)
             elif self.log_transform == 'e':
                 price = np.log(price)
+                change = np.log(change)
             else:
                 price = np.log10(price)
+                change = np.log10(change)
 
-        sample = {'date': date, 'price':price}
+        sample = {'date_label': date_label, 'price': price, 'change': change}
         return sample
 
+    def roll_forward(self):
+        for i in range(1, len(self.price_history)):
+            if self.price_history.iloc[i, 1] == 0.0:
+                self.price_history.iloc[i, 1] = self.price_history.iloc[i-1, 1]
+        print('Prices carried forward!')
+
+    def calculate_differences(self):
+        self.price_history['change'] = None
+        for i in range(1, len(self.price_history)):
+            self.price_history.iloc[i, 2] = self.price_history.iloc[i, 1] - self.price_history.iloc[i-1, 1]
+        print('Computed periodic price change!')
--- SPY_LSTM.py
+++ SPY_LSTM.py
@@ -1,1 +1,11 @@
 import os
+from torch.utils.data import DataLoader
+import HistoricalPriceDataset as PriceData
+
+pricedata = PriceData.HistPriceDataset(csvfile='./data/SP500.csv')
+
+for row in range(len(pricedata)):
+    record = pricedata[row]
+    print(record)
+
+
