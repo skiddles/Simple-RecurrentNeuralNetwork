0f3ba55dc559710e03fcd27e45072c6d431c4127
skiddles <marks.technical@outlook.com>
2018-08-01 22:28:13 -0500

    Implements dataloader with sliding window.

--- SPY_LSTM.py
+++ SPY_LSTM.py
@@ -1,21 +1,111 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
 from torch.utils.data import DataLoader
-import HistoricalPriceDataset as PriceData
-
+import historicalpricedataset as prices
+import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import optparse
 
 debug_level = 3
-pricedata = PriceData.HistPriceDataset(csvfile='./data/SP500.csv')
+window_size = 20
+focus = 'price'  # options: price or change
 
-if debug_level >= 5:
+pricedata = prices.HistPriceDataset(csvfile='./data/SP500.csv', window=window_size)
+
+if debug_level >= 6:
     for row in range(len(pricedata)):
         record = pricedata[row]
         print(record)
 
 
-dataloader = DataLoader(pricedata,
-                        batch_size=1,
-                        shuffle=False,
-                        num_workers=4)
+class LstmModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.lstm1 = nn.LSTM(1, 51)
+        self.lstm2 = nn.LSTM(51, 51)
+        self.linear = nn.Linear(51, 1)
+
+    def forward(self, input, future=0):
+        outputs = []
+        h_t = torch.zeros(input.size(0), 51, dtype=torch.double)
+        c_t = torch.zeros(input.size(0), 51, dtype=torch.double)
+        h_t2 = torch.zeros(input.size(0), 51, dtype=torch.double)
+        c_t2 = torch.zeros(input.size(0), 51, dtype=torch.double)
+
+        for i, input_t in enumerate(input.chunk(input.size(1), dim=1)):
+            h_t, c_t = self.lstm1(input_t, (h_t, c_t))
+            h_t2, c_t2 = self.lstm2(h_t, (h_t2, c_t2))
+            output = self.linear(h_t2)
+            outputs += [output]
+        for i in range(future):  # if we should predict the future
+            h_t, c_t = self.lstm1(output, (h_t, c_t))
+            h_t2, c_t2 = self.lstm2(h_t, (h_t2, c_t2))
+            output = self.linear(h_t2)
+            outputs += [output]
+
+        outputs = torch.stack(outputs, 1).squeeze(2)
+        return outputs
 
 
-for i, x in enumerate(dataloader):
-    print(x)
+if __name__ == '__main__':
+
+    # set random seeds to 0
+    np.random.seed(0)
+    torch.manual_seed(0)
+    # load data and make training set
+
+    dataloader = DataLoader(pricedata,
+                            num_workers=4)
+
+    if debug_level >= 5:
+        for i, d in enumerate(dataloader):
+            print(i, d)
+
+
+    # input = pricedata
+    # target = torch.from_numpy(data[3:, 1:])
+    # test_input = torch.from_numpy(data[:3, :-1])
+    # test_target = torch.from_numpy(data[:3, 1:])
+    # build the model
+    model = LstmModel()
+    model.double()
+    criterion = nn.MSELoss()
+    # use LBFGS as optimizer since we can load the whole data to train
+    optimizer = optim.LBFGS(model.parameters(), lr=0.8)
+
+    #begin to train
+    for i in range(15):
+        print('STEP: ', i)
+        def closure():
+            optimizer.zero_grad()
+            out = model(input)
+            loss = criterion(out, target)
+            print('loss:', loss.item())
+            loss.backward()
+            return loss
+        optimizer.step(closure)
+        # begin to predict, no need to track gradient here
+        with torch.no_grad():
+            future = 1000
+            pred = model(test_input, future=future)
+            loss = criterion(pred[:, :-future], test_target)
+            print('test loss:', loss.item())
+            y = pred.detach().numpy()
+        # draw the result
+        plt.figure(figsize=(30,10))
+        plt.title('Predict future values for time sequences\n(Dashlines are predicted values)', fontsize=30)
+        plt.xlabel('x', fontsize=20)
+        plt.ylabel('y', fontsize=20)
+        plt.xticks(fontsize=20)
+        plt.yticks(fontsize=20)
+        def draw(yi, color):
+            plt.plot(np.arange(input.size(1)), yi[:input.size(1)], color, linewidth = 2.0)
+            plt.plot(np.arange(input.size(1), input.size(1) + future), yi[input.size(1):], color + ':', linewidth = 2.0)
+        draw(y[0], 'r')
+        draw(y[1], 'g')
+        draw(y[2], 'b')
+        plt.savefig('predict%d.pdf'%i)
+    plt.close()
