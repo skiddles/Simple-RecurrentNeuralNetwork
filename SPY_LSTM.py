f31553b40a60c2b4e269bd950667b0cbcb882be0
skiddles <marks.technical@outlook.com>
2018-09-14 07:11:55 -0500

    unknown changes

--- SPY_LSTM.py
+++ SPY_LSTM.py
@@ -4,108 +4,128 @@ import torch.optim as optim
 from torch.utils.data import DataLoader
 import historicalpricedataset as prices
 import numpy as np
-import matplotlib
-matplotlib.use('Agg')
-import matplotlib.pyplot as plt
-import optparse
 
 debug_level = 3
-window_size = 20
-focus = 'price'  # options: price or change
-
-pricedata = prices.HistPriceDataset(csvfile='./data/SP500.csv', window=window_size)
-
-if debug_level >= 6:
-    for row in range(len(pricedata)):
-        record = pricedata[row]
-        print(record)
+window_size = 15  # The window size includes the target value at position -1
+hidden_size = [6, 3]
+batch_size = 100
+EPOCHS = 200
+epoch_loss = 0.0
+print_at_batch_intervals = False
 
 
 class LstmModel(nn.Module):
+
     def __init__(self):
         super().__init__()
-        self.lstm1 = nn.LSTM(1, 51)
-        self.lstm2 = nn.LSTM(51, 51)
-        self.linear = nn.Linear(51, 1)
+        self.lstm1 = nn.LSTM(input_size=window_size-1, hidden_size=hidden_size[0])
+        self.lstm2 = nn.LSTM(input_size=hidden_size[0], hidden_size=hidden_size[1])
+        self.linear = nn.Linear(hidden_size[1], 1)
 
-    def forward(self, input, future=0):
+    def forward(self, forward_input, future=0):
         outputs = []
-        h_t = torch.zeros(input.size(0), 51, dtype=torch.double)
-        c_t = torch.zeros(input.size(0), 51, dtype=torch.double)
-        h_t2 = torch.zeros(input.size(0), 51, dtype=torch.double)
-        c_t2 = torch.zeros(input.size(0), 51, dtype=torch.double)
 
-        for i, input_t in enumerate(input.chunk(input.size(1), dim=1)):
-            h_t, c_t = self.lstm1(input_t, (h_t, c_t))
-            h_t2, c_t2 = self.lstm2(h_t, (h_t2, c_t2))
-            output = self.linear(h_t2)
-            outputs += [output]
-        for i in range(future):  # if we should predict the future
-            h_t, c_t = self.lstm1(output, (h_t, c_t))
-            h_t2, c_t2 = self.lstm2(h_t, (h_t2, c_t2))
-            output = self.linear(h_t2)
-            outputs += [output]
+        h_t1 = torch.randn(forward_input.size(1), forward_input.size(1),
+                           hidden_size[0], dtype=torch.double)
+        c_t1 = torch.zeros(forward_input.size(1), forward_input.size(1),
+                           hidden_size[0], dtype=torch.double)
+        h_t2 = torch.randn(h_t1.size(0), forward_input.size(1),
+                           hidden_size[1], dtype=torch.double)
+        c_t2 = torch.zeros(c_t1.size(0), forward_input.size(1),
+                           hidden_size[1], dtype=torch.double)
 
-        outputs = torch.stack(outputs, 1).squeeze(2)
+        for ix, input_t in enumerate(forward_input.chunk(forward_input.size(1), dim=2)):
+            # if ix == 0:
+            #     print("Input is of size %s" % str(input_t.size()))
+            #     print(input[:, 0, :])
+            # print(input_t.size(0), input_t.size(1), input_t.size(2))
+            # h_t1, c_t1 = self.lstm1(input_t)
+            h_t1, c_t1 = self.lstm1(input_t, (h_t1, c_t1))
+            # print(c_t1)
+            # print("Layer 1: %i, %i" % (len(h_t1), len(c_t1)))
+
+            # h_t2, c_t2 = self.lstm2(h_t1)
+            h_t2, c_t2 = self.lstm2(h_t1, (h_t2, c_t2))
+            # print("Layer 2: %i, %i" % (len(h_t2), len(c_t2)))
+            # print(h_t2.size())
+            output = self.linear(h_t2)
+            # if ix == 0:
+            #     print("output is of type: %s and of size %s" % (type(output), output.size()))
+            #     print(output[:, 0, 0])
+            outputs.extend([output])
+        # print("outputs is of type: %s" % type(outputs))
+        # print("Elements in outputs are of type: %s and of size %s" % (outputs[0], outputs[0].size()))
+        outputs = torch.stack(outputs, 1)
+        # print("Pre-squeeze outputs is of size: %s" % str(outputs.size()))
+        outputs = outputs.squeeze(3)
+        # print()
+        # print("Returned outputs is of type: %s" % type(outputs))
+        # print("Returned outputs is of size: %s" % str(outputs.size()))
+        # exit()
         return outputs
 
 
 if __name__ == '__main__':
-
     # set random seeds to 0
     np.random.seed(0)
     torch.manual_seed(0)
-    # load data and make training set
 
-    dataloader = DataLoader(pricedata,
-                            num_workers=4)
+    # Create the Dataset Object
+    pricedata = prices.HistPriceDataset(csvfile='./data/S&P 500 Historical Data.csv',
+                                        window=window_size,
+                                        log=False)
+
+    if debug_level >= 6:
+        for row in range(len(pricedata)):
+            record = pricedata[row]
+            print(record)
+
+    # Create the data loader
+    dataloader = DataLoader(pricedata, batch_size=batch_size, shuffle=True, num_workers=4)
 
     if debug_level >= 5:
         for i, d in enumerate(dataloader):
+            if i == 0:
+                print(len(d))
             print(i, d)
 
-
-    # input = pricedata
-    # target = torch.from_numpy(data[3:, 1:])
-    # test_input = torch.from_numpy(data[:3, :-1])
-    # test_target = torch.from_numpy(data[:3, 1:])
     # build the model
-    model = LstmModel()
-    model.double()
+    model = LstmModel().double()
+
     criterion = nn.MSELoss()
     # use LBFGS as optimizer since we can load the whole data to train
-    optimizer = optim.LBFGS(model.parameters(), lr=0.8)
+    # optimizer = optim.LBFGS(model.parameters(), lr=0.001)
+    # optimizer = optim.Adam(model.parameters(), weight_decay=0.1, lr=0.01)
+    optimizer = optim.SGD(model.parameters(), weight_decay=0.1, lr=0.005)
 
-    #begin to train
-    for i in range(15):
-        print('STEP: ', i)
-        def closure():
+
+    # Begin training
+    for i in range(EPOCHS):
+        epoch_loss = 0.0
+        print('EPOCH: ', i)
+        # max_j = len(dataloader)-1
+        for j, open_batch in enumerate(dataloader):
+            print(open_batch.shape)
+            # print("J: %i" % max_j)
+            # print("Batch is of type: %s and has a shape of %s" % (type(batch), batch.shape))
+            input = open_batch[:, 0:-1].unsqueeze_(1)
+            # print("Input is of type: %s and has a shape of %s" % (type(input), input.shape))
+            target = open_batch[:, -1:].unsqueeze_(1)
+            # print("Target is of type: %s and has a shape of %s" % (type(target), target.shape))
+
             optimizer.zero_grad()
             out = model(input)
+            # print("Out size: %s" % str(out.size()))
+            # print(out)
+            # print("Target size: %s" % str(target.size()))
             loss = criterion(out, target)
-            print('loss:', loss.item())
+            # print('Epoch: %i, Batch ID: %i,  loss: %0.7f, epoch loss: %0.3f' % (i, j, loss.item(), epoch_loss))
+            if (j + 1) % 10 == 0 or j == max_j:
+                if print_at_batch_intervals:
+                    print('Epoch: %i, Batch ID: %i,  loss: %0.7f' % (i, j, loss.item()))
+                # print('Epoch: %i, Batch ID: %i,  loss: %0.7f' % (i, j, np.log10(loss.item())))
+            epoch_loss += float(loss.item())
             loss.backward()
-            return loss
-        optimizer.step(closure)
-        # begin to predict, no need to track gradient here
-        with torch.no_grad():
-            future = 1000
-            pred = model(test_input, future=future)
-            loss = criterion(pred[:, :-future], test_target)
-            print('test loss:', loss.item())
-            y = pred.detach().numpy()
-        # draw the result
-        plt.figure(figsize=(30,10))
-        plt.title('Predict future values for time sequences\n(Dashlines are predicted values)', fontsize=30)
-        plt.xlabel('x', fontsize=20)
-        plt.ylabel('y', fontsize=20)
-        plt.xticks(fontsize=20)
-        plt.yticks(fontsize=20)
-        def draw(yi, color):
-            plt.plot(np.arange(input.size(1)), yi[:input.size(1)], color, linewidth = 2.0)
-            plt.plot(np.arange(input.size(1), input.size(1) + future), yi[input.size(1):], color + ':', linewidth = 2.0)
-        draw(y[0], 'r')
-        draw(y[1], 'g')
-        draw(y[2], 'b')
-        plt.savefig('predict%d.pdf'%i)
-    plt.close()
+
+            optimizer.step()
+        print("Loss for epoch = %0.4f" % epoch_loss)
--- historicalpricedataset.py
+++ historicalpricedataset.py
@@ -1,12 +1,12 @@
 import torch
 from torch.utils.data.dataset import Dataset
-import pandas as pd
 import numpy as np
-
+import pandas as pd
+import random
 
 class HistPriceDataset(Dataset):
     """Historical Stock Price Dataset"""
-    def __init__(self, csvfile, window=30, log=False):
+    def __init__(self, csvfile, window=30, log=False, valid_pct=0.2):
         """
         :param csvfile:
         :param window:
@@ -18,57 +18,60 @@ class HistPriceDataset(Dataset):
         self.qual_filename = csvfile
         self.log_transform = log
 
+        rawfram = pd.read_csv(self.qual_filename, header=0, thousands=r',')
+
         # Load the data
-        self.price_history_frame = pd.read_csv(self.qual_filename)
+        self.rawprices = rawfram['Price'].as_matrix()
+
+        self.open = rawfram['Open'].as_matrix()
+
+        self.high = rawfram['High'].as_matrix()
+
+        self.low = rawfram['Low'].as_matrix()
+
+        self.date_strings = rawfram['Date']
+
+        x = int((1.0-valid_pct)*len(self.rawprices))
+        self.is_train_set = x
 
         # Apply standard transformations
         self.roll_forward()
-        self.calculate_differences()
-        self.date_labels, self.prices, self.change = self.torch_it(False)
+
+        # self.rawprices = np.concatenate((self.rawprices, self.is_train_set), axis=2)
 
     def __len__(self):
-        return len(self.price_history_frame) - self.window
+        return len(self.rawprices) - self.window
 
     def __getitem__(self, idx):
-        date_label = self.date_labels.iloc[idx+self.window]
-        price = self.prices[idx:idx+self.window]
-        change = self.change[idx:idx+self.window]
+        ix = int(idx)+self.window
+        validation_ix_start = self.is_train_set
+        print(self.rawprices.shape)
+        price = self.rawprices[idx:ix]
+        if idx<=validation_ix_start:
+            train_price = self.rawprices[idx:ix]
+        else:
+            validation_price = self.rawprices[idx:ix]
 
         if self.log_transform:
             if self.log_transform == 10:
                 price = np.log10(price)
-                change = np.log10(change)
             elif self.log_transform == 2:
                 price = np.log2(price)
-                change = np.log2(change)
             elif self.log_transform == 'e':
                 price = np.log(price)
-                change = np.log(change)
             else:
                 price = np.log10(price)
-                change = np.log10(change)
-        sample = {'date_label': date_label, 'price': price, 'change': change}
-        return sample
+        return train_price, validation_price
 
     def roll_forward(self):
-        for i in range(1, len(self.price_history_frame)):
-            if self.price_history_frame.iloc[i, 1] == 0.0:
-                self.price_history_frame.iloc[i, 1] = self.price_history_frame.iloc[i-1, 1]
+        for i in range(1, len(self.rawprices)):
+            if self.rawprices[i] == 0.0:
+                self.rawprices[i] = self.rawprices[i-1]
+            if self.open[i] == 0.0:
+                self.open[i] = self.open[i-1]
+            if self.high[i] == 0.0:
+                self.high[i] = self.high[i-1]
+            if self.low[i] == 0.0:
+                self.low[i] = self.low[i-1]
         print('Prices carried forward!')
 
-    def calculate_differences(self):
-        self.price_history_frame['change'] = np.nan
-        for i in range(1, len(self.price_history_frame)):
-            self.price_history_frame.iloc[i, 2] = np.float64((self.price_history_frame.iloc[i-1, 1] - self.price_history_frame.iloc[i, 1]) / self.price_history_frame.iloc[i-1, 1])
-        print('Computed periodic price change!')
-
-    def torch_it(self, convert):
-        datelabels = self.price_history_frame.iloc[:, 0]
-        if convert:
-            prices = torch.from_numpy(np.array(self.price_history_frame.iloc[:, 1]))
-            change = torch.from_numpy(np.array(self.price_history_frame.iloc[:, 2]))
-        else:
-            prices = self.price_history_frame.iloc[:, 1].as_matrix()
-            change = self.price_history_frame.iloc[:, 2].as_matrix()
-
-        return datelabels, prices, change
